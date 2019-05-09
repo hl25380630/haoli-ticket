3568bf5226a47901fd62888a6daf107fdffa3696
lihao <lihao_100@boe.com.cn>
2019-05-09 17:42:25 +0800

    ai

--- src/main/java/com/haoli/ticket/service/SeleniumService.java
+++ src/main/java/com/haoli/ticket/service/SeleniumService.java
@@ -1,10 +1,15 @@
 package com.haoli.ticket.service;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.apache.http.cookie.Cookie;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,24 +37,31 @@ public class SeleniumService {
     public void test() throws Exception {
         System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");
         ChromeOptions options=new ChromeOptions();
+        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
         ChromeDriver browser = new ChromeDriver(options);
         browser.manage().window().maximize();
         String loginUrl = "https://passport.damai.cn/login?ru=https%3A%2F%2Fwww.damai.cn%2F" ;
         browser.get(loginUrl);
-//        List<Cookie> cookieList = damaiClient.getCookieList();
-//        for(Cookie cookie : cookieList) {
-//        	String name = cookie.getName();
-//        	String value = cookie.getValue();
-//        	String path = cookie.getPath();
-//        	org.openqa.selenium.Cookie scookie = new org.openqa.selenium.Cookie(name, value);
-//        	browser.manage().addCookie(scookie);
-//        }
-//        browser.navigate().refresh();
         Thread.sleep(1000);
+        browser.switchTo().frame("alibaba-login-box");
         
         
+//        WebElement dragger = browser.findElement(By.cssSelector("#nc_1_n1z"));
+//        if(dragger!=null) {
+//            Actions action = new Actions(browser);
+//            action.clickAndHold(dragger).build().perform();
+//            int total = 600;
+//            for (int i = 0; i < total; i++) {
+//            	if(i%2==0) {
+//            		action.moveByOffset(2, 0).perform();
+//            	}else {
+//            		action.moveByOffset(-1, 0).perform();
+//            	}
+//                
+//            }
+//        }
         
-        
+        Thread.sleep(1000);
         browser.close();
         browser.quit();
     }
